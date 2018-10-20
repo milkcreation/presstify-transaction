e87f0cf6cc2ed5a27daa08190143f1bdb9f452a8
Jordy Manner <jordy.manner@milkcreation.fr>
2018-10-20 07:25:16 +0000

    transaction --branch 2.0 -- adaptation evol tiFy

--- Transaction.php
+++ Transaction.php
@@ -6,7 +6,7 @@
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package presstify-plugins/transaction
  * @namespace \tiFy\Plugins\Transaction
- * @version 2.0.0
+ * @version 2.0.1
  */
 
 namespace tiFy\Plugins\Transaction;
