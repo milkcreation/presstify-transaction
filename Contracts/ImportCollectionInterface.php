997fd30223cdd30827c2261d6e9031994aa12562
Jordy Manner <jordy.manner@milkcreation.fr>
2018-11-27 08:00:47 +0000

    transaction -- tag 2.0.5 -- tify routing

---
+++ Contracts/ImportCollectionInterface.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace tiFy\Plugins\Transaction\Contracts;
+
+use tiFy\Contracts\Kernel\Logger;
+
+interface ImportCollectionInterface
+{
+    /**
+     * Récupération de la liste des éléments
+     *
+     * @return ImportItemInterface[]
+     */
+    public function all();
+
+    /**
+     * Récupération des données d'un élément.
+     *
+     * @param string $key Identifiant de qualification de l'élément.
+     *
+     * @return ImportItemInterface
+     */
+    public function get($key);
+
+    /**
+     * Traitement de l'import de la liste des éléments.
+     *
+     * @return array Rapports des résultats de traitement des éléments.
+     */
+    public function import();
+
+    /**
+     * Traitement de l'import d'un élément.
+     *
+     * @param array $item Données de l'élément.
+     *
+     * @return array Rapport de traitement de l'élément.
+     */
+    public function importItem($item);
+
+    /**
+     * Récupération de l'instance du controleur de journalisation.
+     *
+     * @return Logger
+     */
+    public function log();
+
+    /**
+     * Traitement de la liste des éléments.
+     *
+     * @param array $items Liste des éléments.
+     *
+     * @return ImportItemInterface[]
+     */
+    public function parse($items = []);
+}
