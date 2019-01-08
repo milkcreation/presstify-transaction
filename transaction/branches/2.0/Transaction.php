6c04070302d8b430f8ce679a7d6edbc33dda68ed
julien.picard <julien.picard@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-01-08 18:11:20 +0000

    transaction -- branch 1.0 -- correctifs bugs notices ItemWpPostController+ écritures break n

--- transaction/branches/2.0/Transaction.php
+++ transaction/branches/2.0/Transaction.php
@@ -6,7 +6,7 @@
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package presstify-plugins/transaction
  * @namespace \tiFy\Plugins\Transaction
- * @version 2.0.6
+ * @version 2.0.7
  */
 
 namespace tiFy\Plugins\Transaction;
