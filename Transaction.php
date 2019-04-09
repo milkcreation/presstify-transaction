aefa72878c9d5bf8e124accaa2725653607b0265
Jordy Manner <jordy.manner@milkcreation.fr>
2019-04-09 13:21:28 +0000

    transaction -- tag 2.0.14 -- Maj tiFy

---
+++ Transaction.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace tiFy\Plugins\Transaction;
+
+/**
+ * Class Transaction
+ *
+ * @desc Extension PresstiFy de gestion de données de transaction.
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @package tiFy\Plugins\Transaction
+ * @version 2.0.14
+ *
+ * USAGE :
+ * Activation :
+ * ---------------------------------------------------------------------------------------------------------------------
+ * Dans config/app.php ajouter \tiFy\Plugins\Transaction\TransactionServiceProvider à la liste des fournisseurs de
+ * services chargés automatiquement par l'application.
+ * ex.
+ * <?php
+ * ...
+ * use tiFy\Plugins\Transaction\TransactionServiceProvider;
+ * ...
+ *
+ * return [
+ *      ...
+ *      'providers' => [
+ *          ...
+ *          TransactionServiceProvider::class
+ *          ...
+ *      ]
+ * ];
+ *
+ * Configuration :
+ * ---------------------------------------------------------------------------------------------------------------------
+ * Dans le dossier de config, créer le fichier transaction.php
+ * @see /vendor/presstify-plugins/transaction/Resources/config/transaction.php Exemple de configuration
+ */
+final class Transaction
+{
+    use TransactionResolverTrait;
+}
