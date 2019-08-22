4680ca9f36b47c4cb32074b7e9e128ff886a0afe
Jordy Manner <jordy.manner@milkcreation.fr>
2019-08-22 07:39:27 +0000

    transaction -- branch 2.0 -- correctif dépendances

--- Transaction.php
+++ Transaction.php
@@ -15,7 +15,7 @@ use tiFy\Support\Manager;
  * @desc Extension PresstiFy de gestion de données de transaction.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package tiFy\Plugins\Transaction
- * @version 2.0.19
+ * @version 2.0.20
  *
  * USAGE :
  * Activation :
