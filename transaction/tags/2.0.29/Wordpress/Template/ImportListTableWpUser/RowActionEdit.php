24b161019c87e43f267a11047295cef3d6e58119
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-09-09 08:12:17 +0000

    transaction -- tag 2.0.29 -- Correctif Template Lis Offset + Phase #1 Import saveInfos

---
+++ transaction/tags/2.0.29/Wordpress/Template/ImportListTableWpUser/RowActionEdit.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Plugins\Transaction\Wordpress\Template\ImportListTableWpUser;
+
+use tiFy\Plugins\Transaction\Template\ImportListTable\RowActionEdit as BaseRowActionEdit;
+
+class RowActionEdit extends BaseRowActionEdit
+{
+    /**
+     * Instance du gabarit associé.
+     * @var Factory
+     */
+    protected $factory;
+
+    /**
+     * @inheritDoc
+     */
+    public function defaults(): array
+    {
+        return array_merge(parent::defaults(), [
+            'url'     => function (Item $item) {
+                return $item->exists() ? url_factory(get_edit_user_link($item->getId())): null;
+            },
+        ]);
+    }
+}
