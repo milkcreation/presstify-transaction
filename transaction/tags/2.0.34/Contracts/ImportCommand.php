c89f33ecdb51b3fc3f3ce47057b8b92371ef7481
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-09-23 15:44:31 +0000

    transaction -- tag 2.0.34 -- Correctif assets

---
+++ transaction/tags/2.0.34/Contracts/ImportCommand.php
@@ -0,0 +1,82 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Plugins\Transaction\Contracts;
+
+use DateTimeZone;
+use tiFy\Contracts\{
+    Console\Command,
+    Support\ParamsBag
+};
+
+interface ImportCommand extends Command
+{
+    /**
+     * Récupération de la date au format datetime.
+     *
+     * @param int|null $time
+     * @param DateTimeZone|null $tz
+     *
+     * @return string
+     */
+    public function getDate(?int $time = null, ?DateTimeZone $tz = null): string;
+
+    /**
+     * Récupération du format d'affichage des dates.
+     *
+     * @return string
+     */
+    public function getDateFormat(): string;
+
+    /**
+     * Récupération du niveau d'affichage des alertes.
+     *
+     * @return int
+     */
+    public function getLevel(): int;
+
+    /**
+     * Récupération des messages de sortie ou d'un message de sortie.
+     *
+     * @param string|array|null $key Clé d'indice du message
+     * @param mixed $default Valeur de retour par défaut
+     * @param string[] ...$args Liste des arguments dynamique de remplacement
+     *
+     * @return string|ParamsBag
+     */
+    public function messages($key = null, string $default = '', ...$args);
+
+    /**
+     * Récupération de paramètre|Définition de paramètres|Instance du gestionnaire de paramètre.
+     *
+     * @param string|array|null $key Clé d'indice du paramètre à récupérer|Liste des paramètre à définir.
+     * @param mixed $default Valeur de retour par défaut lorsque la clé d'indice est une chaine de caractère.
+     *
+     * @return mixed|ParamsBag
+     */
+    public function params($key = null, $default = null);
+
+    /**
+     * Récupération de l'instance du controleur d'enregistrements.
+     *
+     * @return ImportRecords|null
+     */
+    public function records(): ?ImportRecords;
+
+    /**
+     * Définition de la liste des paramètres.
+     *
+     * @param array $params
+     *
+     * @return static
+     */
+    public function setParams(array $params): ImportCommand;
+
+    /**
+     * Définition de l'instance des enregistrements.
+     *
+     * @param ImportRecords $records
+     *
+     * @return static
+     */
+    public function setRecords(ImportRecords $records): ImportCommand;
+}
