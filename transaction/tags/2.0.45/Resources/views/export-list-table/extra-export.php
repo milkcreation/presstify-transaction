4d0e335f7313080e20c243b719d1011ebc5a4c30
mast3rm1lk <mast3rm1lk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2020-04-03 12:18:12 +0000

    transaction -- 2.0.45 -- Correctif de commande d'import Wordpress

---
+++ transaction/tags/2.0.45/Resources/views/export-list-table/extra-export.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Interface de lancement de l'export.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Contracts\Template\FactoryViewer $this
+ */
+?>
+<?php echo partial('tag', $this->get('button', [])); ?>
+
+<?php echo $this->get('handler', '');
