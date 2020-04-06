f7d88bad721f0e5cc2c9df075cabad6e29217c68
Jordy Manner <jordy.manner@milkcreation.fr>
2020-04-06 05:52:19 +0000

    transaction -- 2.0.46 -- rel_id >> relation

---
+++ Contracts/Transaction.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Plugins\Transaction\Contracts;
+
+use Exception;
+use Psr\Container\ContainerInterface as Container;
+use Symfony\Component\Console\Application as ConsoleApplication;
+
+interface Transaction
+{
+    /**
+     * Récupération de l'instance du gestionnaire d'application CLI.
+     *
+     * @return ConsoleApplication
+     */
+    public function getConsoleApp(): ConsoleApplication;
+
+    /**
+     * Récupération de l'instance du conteneur d'injection de dépendances.
+     *
+     * @return Container|null
+     */
+    public function getContainer(): ?Container;
+
+    /**
+     * Récupération du chemin absolu vers le répertoire des ressources.
+     *
+     * @param string|null $path Chemin relatif d'une resource (répertoire|fichier).
+     *
+     * @return string
+     */
+    public function dir(string $path = null): string;
+
+    /**
+     * Récupération de l'instance du gestionnaire d'import.
+     *
+     * @return ImportManager|null
+     */
+    public function import(): ?ImportManager;
+
+    /**
+     * Résolution de service.
+     *
+     * @param string $alias Nom de qualification du service
+     *
+     * @return object|mixed|null
+     *
+     * @throws Exception
+     */
+    public function resolve(string $alias): ?object;
+
+    /**
+     * Définition de l'instance du conteneur d'injection de dépendances.
+     *
+     * @param Container $container
+     *
+     * @return static
+     */
+    public function setContainer(Container $container): Transaction;
+
+    /**
+     * Récupération de l'url absolue vers le répertoire des ressources.
+     *
+     * @param string|null $path Chemin relatif d'une resource (répertoire|fichier).
+     *
+     * @return string
+     */
+    public function url(string $path = null): string;
+}
