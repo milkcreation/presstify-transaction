093549650f7db885d2c8f5909d0e0f051269d8dc
mast3rm1lk <mast3rm1lk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2020-08-04 17:26:25 +0000

    transaction -- 2.0.55 -- Suppression du cache Wordpress et Woocommerce

---
+++ transaction/tags/2.0.55/Wordpress/Template/ExportListTableWpPost/Params.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Plugins\Transaction\Wordpress\Template\ExportListTableWpPost;
+
+use tiFy\Template\Templates\PostListTable\Params as BaseParams;
+
+class Params extends BaseParams
+{
+    /**
+     * @inheritDoc
+     */
+    public function defaults(): array
+    {
+        return array_merge(parent::defaults(), [
+            'extras'       => ['export'],
+        ]);
+    }
+}
