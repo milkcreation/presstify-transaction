702c1336b54f7945b2e40bac2e99bef3a48fa093
mast3rm1lk <mast3rm1lk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2020-08-04 17:21:09 +0000

    transaction -- 2.0.55 -- Suppression du cache Wordpress et Woocommerce

--- transaction/branches/2.0/Wordpress/ImportWcProduct.php
+++ transaction/branches/2.0/Wordpress/ImportWcProduct.php
@@ -4,9 +4,10 @@ namespace tiFy\Plugins\Transaction\Wordpress;
 
 use tiFy\Plugins\Transaction\{
     Contracts\ImportRecord as BaseImportRecordContract,
+    Wordpress\Contracts\ImportWpPost as ImportWpPostContract,
     Wordpress\Contracts\ImportWcProduct as ImportWcProductContract
 };
-use WP_Post, WC_Product, WC_Product_Simple, WC_Product_Variable, WC_Product_Variation;
+use WP_Post, WC_Product, WC_Cache_Helper, WC_Product_Simple, WC_Product_Variable, WC_Product_Variation;
 
 class ImportWcProduct extends ImportWpPost implements ImportWcProductContract
 {
@@ -15,6 +16,30 @@ class ImportWcProduct extends ImportWpPost implements ImportWcProductContract
      * @var WC_Product|WC_Product_Simple|WC_Product_Variable|WC_Product_Variation|null
      */
     protected $product;
+
+    /**
+     * @inheritDoc
+     */
+    public function clearCache(): ImportWpPostContract
+    {
+        parent::clearCache();
+
+        if ($this->exists() instanceof WP_Post) {
+            $id = $this->exists()->ID;
+
+            wp_cache_delete("product-{$id}", 'products');
+
+            $cache_key = WC_Cache_Helper::get_cache_prefix("product_{$id}") . "_type_{$id}";
+            wp_cache_delete($cache_key, 'products');
+
+            $cache_key = WC_Cache_Helper::get_cache_prefix('products') . WC_Cache_Helper::get_cache_prefix("object_{$id}") . "object_meta_{$id}";
+            wp_cache_delete($cache_key, 'products');
+
+            $this->product = wc_get_product($this->exists());
+        }
+
+        return $this;
+    }
 
     /**
      * @inheritDoc
@@ -33,8 +58,8 @@ class ImportWcProduct extends ImportWpPost implements ImportWcProductContract
     {
         parent::setExists($exists);
 
-        if ($this->exists instanceof WP_Post) {
-            $this->product = wc_get_product($this->exists);
+        if ($this->exists() instanceof WP_Post) {
+            $this->product = wc_get_product($this->exists());
         }
 
         return $this;
