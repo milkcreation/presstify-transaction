ce6ac8493bc919dbe9eac2e19b7c598883afa5bf
mast3rm1lk <mast3rm1lk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2020-08-04 08:56:41 +0000

    transaction -- 2.0.53 -- Correctif mineurs (syntaxe)

---
+++ transaction/tags/2.0.53/Wordpress/Template/ImportListTableWpPost/RowActionEdit.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Plugins\Transaction\Wordpress\Template\ImportListTableWpPost;
+
+use tiFy\Plugins\Transaction\Template\ImportListTable\RowActionEdit as BaseRowActionEdit;
+use tiFy\Support\Proxy\Url;
+
+class RowActionEdit extends BaseRowActionEdit
+{
+    /**
+     * Instance du gabarit associé.
+     * @var Factory
+     */
+    protected $factory;
+
+    /**
+     * @inheritDoc
+     */
+    public function defaults(): array
+    {
+        return array_merge(parent::defaults(), [
+            'url'     => function (Item $item) {
+                return $item->exists() ? Url::set($item->getEditUrl()): null;
+            },
+        ]);
+    }
+}
