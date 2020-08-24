bbab4837d996fd4a3f4da15c6180c5fbc28234f7
Jordy Manner <jordy.manner@milkcreation.fr>
2020-08-24 08:58:05 +0000

    transaction -- Compatibilité restatic 2.0

--- Proxy/Transaction.php
+++ Proxy/Transaction.php
@@ -25,7 +25,7 @@ class Transaction extends AbstractProxy
     /**
      * @inheritDoc
      */
-    public static function getInstanceIdentifier()
+    public static function getInstanceIdentifier(): string
     {
         return 'transaction';
     }
